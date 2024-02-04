aec913fe994be8ada5ffb8f906f8020f025b98a8
Serge <Serg_sh_@mail.ru>
2024-02-04 18:24:40 +0300

    Ver. 11.71

--- lesson_11/6. Knight's_move.py
+++ lesson_11/6. Knight's_move.py
@@ -1,7 +1,22 @@
+# Функция вывода игрового поля
+def graficBoardImput (axis_x, axis_y):
+    chess_square = "|_|"
+    horse = "|N|"
+    board = ""
+    for y in range(8):
+        if y == int(axis_y):
+            for x in range(8):
+                if x == int(axis_x):
+                    board = board + horse
+                else:
+                    board = board + chess_square
+            board = board + "\n"
+        else:
+            board = board + (chess_square * 8) + "\n"
+    print("Разположение на доске")
+    print(board)
+
 # Условное обозначение позиции на поле
-chess_square = "|_|"
-horse = "|N|"
-board = ""
 # Ввод начальных данных
 horse_x = float(input("Введите расположение коня по оси (X): "))
 horse_y = float(input("Введите расположение коня по оси (y): "))
@@ -11,19 +26,7 @@ while (horse_y > 8 or horse_y < 0) or (horse_x > 8 or horse_x < 0):
     horse_x = float(input("По оси (x): "))
     horse_y = float(input("По оси (y): "))
 # Вывод таблицы с условным обозначением
-for y in range(8):
-    if y == int(horse_y):
-        for x in range(8):
-            if x == int(horse_x):
-                board = board + horse
-            else:
-                board = board + chess_square
-        board = board + "\n"
-    else:
-        board = board + (chess_square * 8) + "\n"
-print("Разположение на доске")
-print(board)
-board = ""
+graficBoardImput(horse_x,horse_y)
 # Ввод новой координаты
 print("Введите координаты куда хотите переставить коня")
 new_point_x = float(input("По оси (x): "))
@@ -37,22 +40,10 @@ while (new_point_y > 8 or new_point_y < 0) or (new_point_x > 8 or new_point_x < 
 dx = abs(int(horse_x) - int(new_point_x))
 dy = abs(int(horse_y) - int(new_point_y))
 # Проверка - отладка
-#print("dx=",dx,"dy=",dy)
 # Сравнение и вывод если ход доступен
 if (dx == 1 and dy == 2) or (dx == 2 and dy == 1):
     print("Ход конем возможен")
-    for y in range(8):
-        if y == int(new_point_y):
-            for x in range(8):
-                if x == int(new_point_x):
-                    board = board + horse
-                else:
-                    board = board + chess_square
-            board = board + "\n"
-        else:
-            board = board + (chess_square * 8) + "\n"
-    print("Разположение на доске")
-    print(board)
+    graficBoardImput(new_point_x,new_point_y)
 else:
     print("Так конь ходить не может")
 
