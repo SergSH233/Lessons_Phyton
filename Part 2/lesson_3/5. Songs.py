a3b9a0588c3bb16a9d4a95368821b701760fcbc0
Sergey Shvedov <Serg_sh_@mail.ru>
2024-02-13 12:12:56 +0300

    Ver 2.3.6

--- Part 2/lesson_3/5. Songs.py
+++ Part 2/lesson_3/5. Songs.py
@@ -4,9 +4,17 @@
 #   - выводить сообщение если песни нет в имеющемся списке
 # 3. Вывод в консоль кол-во песен и их общую продолжительность.
 
+def FloatInMin(float):
+    sec = 0.6
+    minute = int((float * 100) // 100)
+    sec_1 = ((float * 100) % 100)
+    sec = int(sec_1 * sec)
+    return print("Длинна плей-листа:",minute,"min", round(sec,2),"sec")
+
 # Альбом взят из задания
 # Depeche Mode - Violator
-
+calc = 1
+total_time = float(0)
 violator_songs = [
     ['World in My Eyes', 4.86],
     ['Sweetest Perfection', 4.43],
@@ -18,18 +26,27 @@ violator_songs = [
     ['Blue Dress', 4.29],
     ['Clean', 5.83]
 ]
-new_pl = []
+new_pl = [['Clean', 5.83],['Waiting for the Night', 6.07],['Policy of Truth', 4.76]]
 pl_size = int(input("Введите кол-во песен нового плей-листа: "))
-# for _ in range(int(input("Введите кол-во песен нового плей-листа: "))):
 while len(new_pl) < pl_size:
+    list_start = len(new_pl)
+    list_finish = 0
     name_song = input("Введите название песни: ")
     for i in range(len(violator_songs)):
         for j in range(len(violator_songs[i])):
             if violator_songs[i][j] == name_song:
-                new_pl.append(violator_songs[i][j])
+                name = violator_songs[i][j]
+                time = violator_songs[i][j + 1]
+                new_pl.append([name,time])
                 print("Добавлена песня: ",violator_songs[i][j])
-                break
-            else:
-                print("Такой песни тут нет")
-print(new_pl)
+    list_finish = len(new_pl)
+    if list_start == list_finish:
+        print("Такой песни нет в вашем списке")
+# print(new_pl)
+for x in range(len(new_pl)):
+    total_time = total_time + float(new_pl[x][1])
+    print(calc,new_pl[x][0],"Time:",new_pl[x][1])
+    calc += 1
+FloatInMin(total_time)
+
 
